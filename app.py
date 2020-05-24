2aa6a22f37c8f3ce6fb6821a0a5495278e3c0259
xiewenhua <vievenvua@gmail.com>
2020-05-24 12:15:54 +0800

    Add static files

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import render_template
 
 app = Flask(__name__)
 
-name = 'Grey Li'
+name = 'Wenhua Xie'
 movies = [    {'title': 'My Neighbor Totoro', 'year': '1988'},    {'title': 'Dead Poets Society', 'year': '1989'},    {'title': 'A Perfect World', 'year': '1993'},    {'title': 'Leon', 'year': '1994'},    {'title': 'Mahjong', 'year': '1996'},    {'title': 'Swallowtail Butterfly', 'year': '1996'},    {'title': 'King of Comedy', 'year': '1999'},    {'title': 'Devils on the Doorstep', 'year': '1999'},    {'title': 'WALL-E', 'year': '2008'},    {'title': 'The Pork of Music', 'year': '2012'},]
 
 @app.route('/')
