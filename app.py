d81149bab701dcac7c474d2133fd432e76111538
xiewenhua <vievenvua@gmail.com>
2020-05-24 18:28:03 +0800

    添加sql

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask
 from flask import url_for
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
 
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix='sqlite:///'
+else:
+    prefix='sqlite:////'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']=prefix+os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+
+
+
+
 
 name = 'Wenhua Xie'
 movies = [    {'title': 'My Neighbor Totoro', 'year': '1988'},    {'title': 'Dead Poets Society', 'year': '1989'},    {'title': 'A Perfect World', 'year': '1993'},    {'title': 'Leon', 'year': '1994'},    {'title': 'Mahjong', 'year': '1996'},    {'title': 'Swallowtail Butterfly', 'year': '1996'},    {'title': 'King of Comedy', 'year': '1999'},    {'title': 'Devils on the Doorstep', 'year': '1999'},    {'title': 'WALL-E', 'year': '2008'},    {'title': 'The Pork of Music', 'year': '2012'},]
@@ -26,3 +41,11 @@ def test_url_for():
     print(url_for('user_page',name='Lisa'))
     print(url_for('test_url_for',num=2))
     return 'Test page'
+
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(20))
+class Movie(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(60))
+    year=db.Column(db.String(4))
